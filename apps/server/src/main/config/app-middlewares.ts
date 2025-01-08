a6add4c73ca12f763a7cdc0765ca12a3b10af8c7
matheus <matheusdev20@gmail.com>
2025-01-08 02:10:04 -0300

    feat: persisting session on the database

--- apps/server/src/main/config/app-middlewares.ts
+++ apps/server/src/main/config/app-middlewares.ts
@@ -1,13 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express, { Express } from 'express';
 import cors from 'cors';
-import session from 'express-session';
+import pg from 'pg';
+import session, { MemoryStore } from 'express-session';
 import passport from 'passport';
 import { jsonParser } from '../middlewares';
 import upload from '../../config/storage/upload';
 
+const PGSession = require('connect-pg-simple')(session);
+
+const pgPool = new pg.Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+});
+const store = process.env.NODE_ENV === 'production' ? new PGSession({ pool: pgPool, tableName: 'session' }) : new MemoryStore();
+
 export default (app: Express): void => {
   app.use(session({
-    secret: 'your-secret-key',
+    store,
+    secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
   }));
