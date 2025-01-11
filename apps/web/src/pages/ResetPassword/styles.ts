b2dbdc2f7cc6cf048ed838425c9e547fe7625fbe
matheus <matheusdev20@gmail.com>
2025-01-10 21:40:08 -0300

    chore: cleaning chakra pages

--- apps/web/src/pages/ResetPassword/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const ResetForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-`
--- apps/web/src/routes/routes.tsx
+++ apps/web/src/routes/routes.tsx
@@ -1,32 +1,12 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import React from "react";
 import { Home } from "../pages/home";
-// import { LoginPage } from "../pages/Login";
-// import { LoginPage } from '../pages/Login'
-// import { SignUpPage } from '../pages/Signup'
-// import { RegisterPetPage } from '../pages/RegisterPet'
-// import { Home } from '../pages/home/'
-// import { PetInfo } from '../pages/PetInfo'
-// import { AboutPage } from '../pages/About'
-// import { ForgotPassword } from '../pages/ForgotPassword'
-// import { ResetPassword } from '../pages/ResetPassword'
 
 export const AppRoutes = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" element={<Home />} />
-      {/* <Route path="/login" element={<LoginPage />} /> */}
-
-      {/* <Route path='/criar-usuario' element={<SignUpPage />} /> */}
-
-      {/* <Route path='/recuperar-senha' element={<ForgotPassword />} /> */}
-
-      {/* <Route path='/resetar-senha' element={<ResetPassword />} /> */}
-
-      {/* <Route path='/cadastrar-pet' element={<RegisterPetPage />} /> */}
-
-      {/* <Route path='/about' element={<AboutPage />} /> */}
 
       {/* <Route path='/pet/:name' element={<PetInfo />} /> */}
     </Routes>
