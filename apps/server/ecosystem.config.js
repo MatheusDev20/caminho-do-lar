db43fc49993d926e815f786769823a06cad8b082
matheus <matheusdev20@gmail.com>
2025-06-07 20:10:02 -0300

    sync

--- apps/server/ecosystem.config.js
+++ apps/server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "cdl-api",
-      script: "dist/server.js",   // caminho para o entry point JS
+      script: "dist/main/server.js",   // caminho para o entry point JS
       cwd: __dirname,             // garante que o processo roda aqui dentro
       env_file: ".env",           // carrega variáveis sensíveis
       instances: 1,               // simples: 1 instância; altere se precisar cluster
